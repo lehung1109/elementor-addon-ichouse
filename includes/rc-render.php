822dbc110c13bf1f5432e5f57ef49cde82a53523
Hung Le <hung.le.ext@ericsson.com>
2026-05-31 17:14:29 +0700

    Add debugging output in eai_rc_render_html function to indicate cache hits for improved performance tracking.

--- includes/rc-render.php
+++ includes/rc-render.php
@@ -97,6 +97,7 @@ if (! function_exists('eai_rc_render_html')) {
       && is_string($cached['html'])
       && $cached['html'] !== ''
     ) {
+      var_dump('cache hit');
       return [
         'html' => $cached['html'],
         'from_cache' => true,
