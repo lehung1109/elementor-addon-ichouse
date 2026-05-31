b3ae4af3fb07736f2081410cacf84201845450da
Hung Le <hung.le.ext@ericsson.com>
2026-05-31 17:31:53 +0700

    Update eai_toc_filter_the_content function to accept widget parameter and change filter hook to improve compatibility with Elementor widgets.

--- includes/table-of-contents.php
+++ includes/table-of-contents.php
@@ -19,8 +19,12 @@ if (! function_exists('eai_toc_should_skip_filter')) {
 }
 
 if (! function_exists('eai_toc_filter_the_content')) {
-  function eai_toc_filter_the_content(string $content): string
+  function eai_toc_filter_the_content(string $content, $widget): string
   {
+    if ('text-editor' !== $widget->get_name()) {
+      return $content;
+    }
+
     if ($content === '' || eai_toc_should_skip_filter()) {
       return $content;
     }
@@ -67,4 +71,4 @@ if (! function_exists('eai_toc_filter_the_content')) {
 }
 
 add_action('init', 'eai_register_frontend_assets');
-add_filter('the_content', 'eai_toc_filter_the_content', 99999999999);
+add_filter('elementor/widget/render_content', 'eai_toc_filter_the_content', 100, 2);
