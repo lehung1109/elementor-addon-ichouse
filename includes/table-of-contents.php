6ad3cb5ef40482e13b726c49a8f26f997436dfd7
Hung Le <hung.le.ext@ericsson.com>
2026-05-31 15:47:39 +0700

    Remove unused processed post IDs logic from eai_toc_filter_the_content function to streamline code and enhance readability.

--- includes/table-of-contents.php
+++ includes/table-of-contents.php
@@ -38,15 +38,6 @@ if (! function_exists('eai_toc_filter_the_content')) {
       return $content;
     }
 
-    static $processed_post_ids = [];
-
-    if (isset($processed_post_ids[$post->ID])) {
-      var_dump('skip because of post is already processed');
-      return $content;
-    }
-    var_dump('go into toc here');
-    $processed_post_ids[$post->ID] = true;
-
     $settings = eai_toc_get_settings();
     $content = eai_toc_add_heading_ids($content);
     $parsed = eai_toc_parse_headings($content);
